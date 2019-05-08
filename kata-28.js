255102068775601c5ecf1b1a966c8e492f4f61c2
Drew <verwiel.code@gmail.com>
2019-05-08 08:55:31 -0600

    kata 28

---
+++ kata-28.js
@@ -0,0 +1,76 @@
+// 28: class - super in constructor
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('Inside a class`s constructor `super()` can be used', () => {
+  it('`extend` a class and use `super()` to call the parent constructor', () => {
+    
+    
+    // class A {constructor() { this.levels = 1; }}
+    // class B {constructor() { this.levels++; }}
+    class A {constructor() { this.levels = 1; }}
+    class B extends A{
+      constructor() {
+        super ()
+        this.levels++;
+      }
+    }
+    //extended class A and added super after the constructor
+    
+    assert.equal(new B().levels, 2);
+  });
+  it('`super()` may also take params', () => {
+    
+    // class A {constructor(startValue=1, addTo=1) { this.counter = startValue + addTo; }}
+    // class B extends A { constructor(...args) { super(); this.counter++; }}
+    class A {constructor(startValue=1, addTo=1) { this.counter = startValue + addTo; }}
+    class B extends A { 
+      constructor(...args) { 
+        super(...args); 
+        this.counter++; 
+      }}
+    // added args params to the super
+
+    assert.equal(new B(42, 2).counter, 45);
+  });
+  it('it is important where you place your `super()` call!', () => {
+    // class A {inc() { this.countUp = 1; }}
+    // class B extends A {
+    //   inc() {
+    //     super.inc();
+    //     this.countUp = 2;
+    //     return this.countUp;
+    //   }
+    // }
+    class A {inc() { this.countUp = 1; }}
+    class B extends A {
+      inc() {
+        this.countUp = 2;
+        super.inc();
+        return this.countUp;
+      }
+    }
+    // super needed to be called after this.countUp
+    
+    assert.equal(new B().inc(), 1);
+  });
+  it('use `super.constructor` to find out if there is a parent constructor', () => {
+    // class ParentClassA {constructor() {"parent"}}
+    // class B extends ParentClassA {
+    //   constructor() {
+    //     super();
+    //     this.isTop = '' + super.konstructer;
+    //   }
+    // }
+    class ParentClassA {constructor() {ParentClassA}}
+    class B extends ParentClassA {
+      constructor() {
+        super();
+        this.isTop = '' + super.constructor;
+      }
+    }
+ 
+    
+    assert(new B().isTop.includes('ParentClassA'), new B().isTop);
+  });
+});
